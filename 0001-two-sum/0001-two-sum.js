5455264cd4d9e27bf3dfe9d3fc4cc9a04dc0c0c1
eunmmm <87576533+eunmmm@users.noreply.github.com>
2024-08-11 11:23:31 +0900

    Time: 83 ms (47.83%), Space: 49.5 MB (73.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,11 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-    let result = [];
-
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
-                result.push(i, j);
+                return [i, j];
             }
         }
     }
-
-    return result;
 };
