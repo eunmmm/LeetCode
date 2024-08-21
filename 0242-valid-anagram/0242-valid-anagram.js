d832dc2491a3f046a10b04e892840faadb6dab8d
eunmmm <87576533+eunmmm@users.noreply.github.com>
2024-08-21 21:58:47 +0900

    Time: 86 ms (16.44%), Space: 52.8 MB (57.46%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    s = s.split("").sort();
+    t = t.split("").sort();
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== t[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
